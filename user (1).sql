307381e2ad1d650396d3f546f69fbcc667eab8c9
MohamedAzizFerchichi <mohamedaziz.ferchichi@esprit.tn>
2023-09-04 18:24:54 +0100

    somestuf

---
+++ user (1).sql
@@ -0,0 +1,5 @@
+
+INSERT INTO `user` (`id`, `email`, `password`, `role`, `username`) VALUES
+(1, 'springtest75@gmail.com', '$2a$10$DaNql7VBYUDNSF.xcxJfRupYJBFYMhe/joGXZTWP1CywXCtJja7sK', 'Fournisseur', 'Esprit Foundation'),
+
+
